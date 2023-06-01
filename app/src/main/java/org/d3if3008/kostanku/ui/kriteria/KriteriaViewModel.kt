e393bfd0fbd77f69c9f296395dbe123403df0644
Yudhistira Haryatmaka Santoso <84625580+disyudhis@users.noreply.github.com>
2023-06-01 10:23:44 +0700

    Memperbaiki index

--- app/src/main/java/org/d3if3008/kostanku/ui/kriteria/KriteriaViewModel.kt
+++ app/src/main/java/org/d3if3008/kostanku/ui/kriteria/KriteriaViewModel.kt
@@ -1,11 +1,9 @@
 package org.d3if3008.kostanku.ui.kriteria
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import org.d3if3008.kostanku.R
 import org.d3if3008.kostanku.model.KamarOptions
-import org.d3if3008.kostanku.model.KategoriKost
 
 class KriteriaViewModel : ViewModel() {
     private var typeCounterKamar = MutableLiveData(0)
@@ -13,32 +11,42 @@ class KriteriaViewModel : ViewModel() {
     private var indexAt = MutableLiveData(0)
 
     private val harga = listOf(
-        KamarOptions(800000, 1000000, 1300000, R.drawable.kecil, R.drawable.sedang, R.drawable.besar),
+        KamarOptions(
+            800000,
+            1000000,
+            1300000,
+            R.drawable.kecil,
+            R.drawable.sedang,
+            R.drawable.besar
+        ),
         KamarOptions(0, 50000, 200000, R.drawable.kosongan, R.drawable.kipas, R.drawable.ac)
     )
 
-//    Mengambil index
+    //    Mengambil index
     fun getIndex(): MutableLiveData<Int> = indexAt
 
-//    Mengambil pilihan tiap options
-    fun getOptions() : KamarOptions {
+    //    Mengambil pilihan tiap options
+    fun getOptions(): KamarOptions {
+        if (harga.size == indexAt.value) {
+            indexAt.value = 0
+        }
         return harga[indexAt.value?.toInt()!!]
     }
 
-//    Nambah index
+    //    Nambah index
     fun increaseIndex() {
         indexAt.value = if (indexAt.value == harga.size) indexAt.value else indexAt.value?.plus(1)
     }
 
-//    Mencari panjang list
+    //    Mencari panjang list
     fun getLengthQuestion(): Int = harga.size
 
-//    Nambah Harga
+    //    Nambah Harga
     fun increaseHargaKamar(point: Int) {
         typeCounterKamar.value = typeCounterKamar.value?.plus(point)
     }
 
-//    Menjumlahkan hasil
+    //    Menjumlahkan hasil
     fun getResult(): Int {
         val kamar = typeCounterKamar.value!!.toInt()
         val fasilitas = typeCounterFasilitas.value!!.toInt()
